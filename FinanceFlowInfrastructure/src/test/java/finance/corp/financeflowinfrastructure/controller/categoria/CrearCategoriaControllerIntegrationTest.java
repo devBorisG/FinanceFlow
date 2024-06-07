c86c064b7b9083d754b133d002592a762f0bc6ec
Helmer Alexander Patiño <80782571+1193090526alexander@users.noreply.github.com>
2024-06-07 09:28:13 -0500

    egreso consultar

---
+++ FinanceFlowInfrastructure/src/test/java/finance/corp/financeflowinfrastructure/controller/categoria/CrearCategoriaControllerIntegrationTest.java
@@ -0,0 +1,38 @@
+package finance.corp.financeflowinfrastructure.controller.categoria;
+
+import finance.corp.financeflowapplication.service.categoria.CrearCategoriaFacade;
+import finance.corp.financeflowinfrastructure.adapter.primary.controller.categoria.CrearCategoriaController;
+import org.junit.jupiter.api.BeforeEach;
+
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+
+    @SpringBootTest(classes = CrearCategoriaController.class)
+
+public class CrearCategoriaControllerIntegrationTest {
+
+
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CrearCategoriaFacade facade;
+
+
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+
+
+}
